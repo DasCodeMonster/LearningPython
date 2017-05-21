96ef8d51816ba908c62c599ae83ccdbd80b90e99
DasCodeMonster <pl.lkw.01@gmail.com>
2017-05-21 23:31:51 +0200

    Modified Multithreading

--- Multithreading.py
+++ Multithreading.py
@@ -8,14 +8,18 @@ class MyThread(threading.Thread):
 		self.Name = Name
 		
 	def run(self):
-		print("Starte ", self.ID)
-		time.sleep(10)
+		lockMe.acquire()
+		print("Starte", self.ID)
+		time.sleep(self.ID*3)
+		lockMe.release()
 		print("Beende", self.ID)
 		
+lockMe = threading.Lock()
 Thread1 = MyThread(1, "Thread1")
 Thread2 = MyThread(2, "Thread2")
 
 Thread1.start()
 Thread2.start()
-
+Thread1.join()
+Thread2.join()
 print("Beende Main Thread")
