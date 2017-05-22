48a201bad8a69db47a27f29ee288d786ae48cde2
DasCodeMonster <pl.lkw.01@gmail.com>
2017-05-22 21:34:59 +0200

    Modefied: Multithreading; Added: Lambda und Map, Logging, Partielle Funktionen

--- Multithreading.py
+++ Multithreading.py
@@ -2,18 +2,20 @@ import threading
 import time
 
 class MyThread(threading.Thread):
+	Ergebnis = [0,1]
 	def __init__(self, ID, Name):
 		threading.Thread.__init__(self)
 		self.ID = ID
 		self.Name = Name
 		
 	def run(self):
-		lockMe.acquire()
-		print("Starte", self.ID)
-		time.sleep(self.ID*3)
-		lockMe.release()
-		print("Beende", self.ID)
-		
+		i=0
+		while i<20:
+			lockMe.acquire()
+			zahl = self.Ergebnis[len(self.Ergebnis)-2] + self.Ergebnis[len(self.Ergebnis)-1]
+			self.Ergebnis.append(zahl)
+			lockMe.release()
+			i = i+1
 lockMe = threading.Lock()
 Thread1 = MyThread(1, "Thread1")
 Thread2 = MyThread(2, "Thread2")
@@ -22,4 +24,5 @@ Thread1.start()
 Thread2.start()
 Thread1.join()
 Thread2.join()
+print(MyThread.Ergebnis[42])
 print("Beende Main Thread")
