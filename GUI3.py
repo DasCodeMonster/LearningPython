e49f94055893600b1145c727c8225b21a92db37b
DasCodeMonster <pl.lkw.01@gmail.com>
2017-05-30 20:15:34 +0200

    Modified: GUI3

--- GUI3.py
+++ GUI3.py
@@ -1,37 +1,40 @@
 import tkinter as tk
 import tkinter.filedialog as tkf
-# def FileOpen():
-#     f = open(tkf.askdirectory(), "w",)
-#
-# def Fenster(root):
-#     root.title("Browse")
-#     tk.Label(root, text="Browse your directory.").pack()
-#     tk.Button(root, text="Browse", command=FileOpen()).pack()
+
+Directory = None
+Filename = None
+
 def DeinText():
     print(e.get())
 
 def Browse():
+    global Directory
+    global Filename
     Directory = tkf.askdirectory()
-    Directory(Directory)
+    # Filename = tkf.asksaveasfilename()
+    d.set(Directory)
+    print(Directory)
 
-def Directory(Directory):
-    Path = Directory
-    return Path
+def dynamiclabel(label):
+    label.config(text = Directory)
 
 def SaveFile():
-    f = open(Directory() + "\\Hallo.txt", "w")
+    f = open(Directory + "\\Hallo.txt", "w")
     f.write(e.get())
 
 if __name__ == "__main__":
     root = tk.Tk()
     root.title("GUI")
+    d = tk.StringVar()
     tk.Label(root, text="Browse your directory.").grid(row=0)
-    tk.Button(root, text="Quit", command=root.quit).grid(row=3)
-    tk.Button(root, text="Save File", command=Directory).grid(row=3, column=1)
-    e = tk.Entry(root)
-    tk.Button(root, text="Browse", command=Browse).grid(row=0, column=2, pady=4, sticky="w")
-    tk.Label(root, text="Dein Text: ").grid(row=2)
-    e.grid(row=2, column=1)
+    tk.Button(root, text="Browse", command=Browse).grid(row=0, column=2)
+    DynamicLabel = tk.Label(root, textvariable=d).grid(row=0, column=1)
+    # dynamiclabel(DynamicLabel)
+    tk.Label(root, text="Dein Text: ").grid(row=2, sticky="w")
     tk.Button(root, text="Print", command=DeinText).grid(row=2, column=2)
+    tk.Button(root, text="Quit", command=root.quit).grid(row=3)
+    tk.Button(root, text="Save File", command=SaveFile).grid(row=3, column=1)
+    e = tk.Entry(root)
+    e.grid(row=2, column=1)
+    root.update_idletasks()
     root.mainloop()
-    # Hallo! Wie geht es Dir?
